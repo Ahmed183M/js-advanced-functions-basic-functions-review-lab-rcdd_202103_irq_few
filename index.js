45eb5ae73e5819f2368cf83cf24de8dadc4bb414
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-14 13:07:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,8 +31,9 @@ const Calculator = {
   },
 }
 
-let array = [Calculator.add, Calculator.divide];
-function actionApplyer(integer, array=array){
+let acts = [Calculator.add, Calculator.divide];
+
+function actionApplyer(integer, array=acts){
   for(const act in array){
     integer = act(integer);
   }
